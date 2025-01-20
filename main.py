7da81198dcf49f495c50e493ebd3bea8b3b08786
delbe <delbenmaxime@gmail.com>
2025-01-20 18:55:50 +0100

    - add view module and to_print function

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from view import to_print
+
+
 def main():
-    print("Hello")
-    print("Hello2")
+    to_print("Hello")
+    to_print("Hello2")
 
 if __name__ == "__main__": #le code sera executé uniquement s'il est appelé
     main()
